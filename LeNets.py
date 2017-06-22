32d9dfdbca621519b237d5701f7e66a80f58aee8
Edgar C Garozn <edgarcgarzon@gmail.com>
2017-06-23 01:32:57 +0200

    Neural network deep increase

---
+++ LeNets.py
@@ -0,0 +1,110 @@
+
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+n_classes = 42
+
+def LeNet2(x, ch = 1):    
+    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+    mu = 0
+    sigma = 0.1
+
+    # Layer 1: Convolutional. Input = 32x32xch. Output = 28x28x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, ch, 12), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(12))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    # Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    # Pooling. Input = 28x28x12. Output = 14x14x12.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 12, 32), mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(32))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    # Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # Pooling. Input = 10x10x32. Output = 5x5x32.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Flatten. Input = 5x5x32. Output = 800.
+    fc0   = flatten(conv2)
+    
+    # Layer 3: Fully Connected. Input = 800. Output = 240.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(800, 240), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(240))
+    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    
+    # Activation.
+    fc1    = tf.nn.relu(fc1)
+
+    # Layer 4: Fully Connected. Input = 240. Output = 84.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(240, 168), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(168))
+    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    
+    # Activation.
+    fc2    = tf.nn.relu(fc2)
+
+    # Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(168, n_classes), mean = mu, stddev = sigma))
+    fc3_b  = tf.Variable(tf.zeros(n_classes))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+    
+    return logits
+
+def LeNet3(x, ch = 1):    
+    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+    mu = 0
+    sigma = 0.1
+    
+    # Layer 1: Convolutional. Input = 32x32xch. Output = 28x28x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, ch, 12), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(12))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    # Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    # Pooling. Input = 28x28x12. Output = 14x14x12.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 12, 32), mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(32))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    # Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # Pooling. Input = 10x10x32. Output = 5x5x32.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Flatten. Input = 5x5x32. Output = 800.
+    fc0   = flatten(conv2)
+    
+    # Layer 3: Fully Connected. Input = 800. Output = 1024.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(800, 1024), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(1024))
+    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    
+    # Activation.
+    fc1    = tf.nn.relu(fc1)
+
+    # Layer 4: Fully Connected. Input = 1024. Output = 1024.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(1024, 1024), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(1024))
+    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    
+    # Activation.
+    fc2    = tf.nn.relu(fc2)
+
+    # Layer 5: Fully Connected. Input = 1024. Output = 10.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(1024, n_classes), mean = mu, stddev = sigma))
+    fc3_b  = tf.Variable(tf.zeros(n_classes))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+    
+    return logits
